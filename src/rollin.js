25ac90c25af090b2fc0dec0111ba995b7a785c31
James Liu <jamesjhliu@gmail.com>
2017-02-12 21:11:49 -0800

    update src documentation

--- src/rollin.js
+++ src/rollin.js
@@ -1,12 +1,10 @@
-// Description
+// Description:
 //   A hubot script that allows for advanced dice rolls.
 //
 // Configuration:
-//   LIST_OF_ENV_VARS_TO_SET
 //
 // Commands:
-//   hubot hello - <what the respond trigger does>
-//   orly - <what the hear trigger does>
+//   hubot roll <sets:?><num?:>d<sides><+/- mod>
 //
 // Notes:
 //   <optional notes required for the script>
